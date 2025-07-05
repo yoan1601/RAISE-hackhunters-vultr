c67289fbc75bbd78cc234a0f3c47c2269f6dbc63
yoan1601 <yoan.rab@gmail.com>
2025-07-05 23:30:02 +0300

    Attempt fix frontenf workflow

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Backend Health Check title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Backend Health Check/i);
+  expect(titleElement).toBeInTheDocument();
 });
